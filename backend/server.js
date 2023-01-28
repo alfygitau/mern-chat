1bcf9c765f7572e1a593f6617a71bca70d407ea8
gitx5622 <ggitau5622@gmail.com>
2023-01-28 15:28:48 +0300

    configured origins

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ const server = app.listen(PORT, () => {
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-chat-e8dm.onrender.com",
   },
   pingTimeout: 60000,
 });
