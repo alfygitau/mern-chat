867fe400f1a7c1792356b5e6df60b3289aa9e25f
gitx5622 <ggitau5622@gmail.com>
2023-01-28 13:58:42 +0300

    final

--- backend/server.js
+++ backend/server.js
@@ -23,25 +23,25 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 // deployment
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname1, "../", "frontend", "build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.resolve(__dirname1, "../", "frontend", "build")));
 
-  console.log(
-    path.resolve(__dirname1, "../", "frontend", "build", "index.html")
-  );
+//   console.log(
+//     path.resolve(__dirname1, "../", "frontend", "build", "index.html")
+//   );
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname1, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname1, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // deployment
 
